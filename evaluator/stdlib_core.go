935cac6fd613ef2be059a8646788ed58cc93e330
Seukwon Kang <kasworld@gmail.com>
2021-01-15 17:04:30 +0900

    update builtinType

--- evaluator/stdlib_core.go
+++ evaluator/stdlib_core.go
@@ -557,31 +557,7 @@ func builtinType(env *object.Environment, args ...object.Object) object.Object {
 		return newError("wrong number of arguments. got=%d, want=1",
 			len(args))
 	}
-	switch args[0].(type) {
-	case *object.String:
-		return &object.String{Value: "string"}
-	case *object.Regexp:
-		return &object.String{Value: "regexp"}
-	case *object.Boolean:
-		return &object.String{Value: "bool"}
-	case *object.Builtin:
-		return &object.String{Value: "builtin"}
-	case *object.File:
-		return &object.String{Value: "file"}
-	case *object.Array:
-		return &object.String{Value: "array"}
-	case *object.Function:
-		return &object.String{Value: "function"}
-	case *object.Integer:
-		return &object.String{Value: "integer"}
-	case *object.Float:
-		return &object.String{Value: "float"}
-	case *object.Hash:
-		return &object.String{Value: "hash"}
-	default:
-		return newError("argument to `type` not supported, got=%s",
-			args[0].Type())
-	}
+	return &object.String{Value: args[0].Type().String()}
 }
 
 // Remove a file/directory.
